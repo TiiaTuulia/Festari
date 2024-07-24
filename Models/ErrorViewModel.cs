906632ef9d517359874ba100616d0dd70c741779
TiiaTuulia <tiiatuulia.viitanen@gmail.com>
2024-07-24 16:50:19 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FestariApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
